80b4775680ef9c569bed9aa52f5e71a672485113
raymosun <sunraysfs@gmail.com>
2023-10-29 12:13:41 -0700

    Implement game logic

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,12 +1,11 @@
-import { useState, useEffect } from 'react';
-import SheepWhite from '../assets/sheep_white.svg';
-import SheepBlue from '../assets/sheep_blue.svg';
-import SheepRed from '../assets/sheep_red.svg';
+import { useState, useEffect, useRef } from 'react';
 import { Sheep, SheepProps } from './Sheep';
 import '../styles/Game.css';
 import { useRecorder } from './Recorder';
 import { useTranscript } from '../lib/useTranscript';
 import { useHume } from '../lib/useHume';
+import prompts from '../lib/prompts.json';
+import groups from '../lib/groups.json';
 
 // Create a new component for the menu
 const Menu = ({ onStart, onInstructions }) => (
@@ -21,14 +20,19 @@ const Menu = ({ onStart, onInstructions }) => (
 export const Game = () => {
   // const vars
   const GAME_CLOCK_PERIOD = 100; // ms
+  const MIN_TIME_BETWEEN_SHEEPS = 8000;
+  const SHEEP_CHANCE = 0.05;
 
   const [intervalId, setIntervalId] = useState<any>(null); // if null, means game loop already running
   const [sheeps, setSheeps] = useState<SheepProps[]>([]);
   const [recording, setRecording] = useState<boolean>(false);
   const [dogImage, setDogImage] = useState("../assets/normal_dog.svg");
+  const [lastSheepTime, setLastSheepTime] = useState(Date.now());
+  const transcriptionRef = useRef<string>();
+  const [finishedSheeps, setFinishedSheeps] = useState<SheepProps[]>([]);
   // const [audioChunks, setAudioChunks] = useState<Blob[]>([]);
   // const [recordingLength, setRecordingLength] = useState(0);
-  
+
   const getEmotions = useHume();
   const onNewAudio = (audioBlob: Blob, recordingStartTime: number) => {
     const recordingLength = Date.now() - recordingStartTime;
@@ -37,6 +41,38 @@ export const Game = () => {
       console.log(emotions);
       const sortedEmotions = (emotions as any).prosody.predictions[0].emotions.sort((a, b) => a.score - b.score);
       console.log(sortedEmotions);
+      setSheeps(sheeps => {
+        // score sheep
+        console.log(groups[sheeps[0].type]);
+        console.log(sortedEmotions[sortedEmotions.length - 1].name);
+        if (sortedEmotions.slice(sortedEmotions.length - 4, sortedEmotions.length).some(emotion => {
+          return groups[sheeps[0].type].includes(emotion.name);
+        })) {
+          console.log('matches')
+          console.log(transcriptionRef.current);
+          let cleanTrans = transcriptionRef.current?.toString();
+          cleanTrans = cleanTrans?.toLowerCase().replace(/\.|,|\?|!/, '');
+          console.log(cleanTrans);
+          if (cleanTrans === sheeps[0].word) {
+            console.log('transcription matches too')
+            setSheeps(prev => {
+              setFinishedSheeps(prevFinished => {
+                return [...prevFinished, {...sheeps[0], y: -100}]
+              })
+              return [...prev.slice(1)]
+            })
+          } else {
+            console.log('transcript doesnt match', 'trans', cleanTrans, 'word', sheeps[0].word)
+          }
+        }
+        else
+          console.log('doesnt match')
+        console.log(sheeps);
+        return sheeps;
+      })
+      testSheep();
+      const currSheep = sheeps[0];
+      console.log(currSheep.type)
     }).catch(error => console.log(error));
   };
 
@@ -47,6 +83,7 @@ export const Game = () => {
   //START BUTTON
   const handleStart = () => {
     setShowMenu(false); 
+    startLoop();
   };
 
   //INSTRUCTIONS
@@ -60,12 +97,11 @@ export const Game = () => {
   useEffect(() => {
     fetch('/env.json').then(res => res.json()).then(data => {
       (window as any).API_KEY = data.REACT_APP_API_KEY;
-      setSheeps([
-        { asset: SheepWhite, progress: 0.5, y: 200},
-        { asset: SheepBlue, progress: 0.2, y: 300},
-        { asset: SheepRed, progress: 0.8, y: 400}
-      ]);
-      startLoop();
+      // setSheeps([
+      //   { asset: SheepWhite, progress: 0.5, y: 200, word:'hello'},
+      //   { asset: SheepBlue, progress: 0.2, y: 300, word:'hello'},
+      //   { asset: SheepRed, progress: 0.8, y: 400, word:'hello'}
+      // ]);
     })
   }, []);
 
@@ -78,12 +114,12 @@ export const Game = () => {
 
   // set up testing data
   useEffect(() => {
-    setSheeps([
-      { asset: SheepWhite, progress: 0, y: 200},
-      { asset: SheepBlue, progress: -0.2, y: 300 },
-      { asset: SheepRed, progress: -0.8, y: 400 },
-    ]);
-    startLoop();
+    // setSheeps([
+    //   { asset: SheepWhite, progress: 0, y: 200, word: 'hi'},
+    //   { asset: SheepBlue, progress: -0.2, y: 300, word: 'hi' },
+    //   { asset: SheepRed, progress: -0.8, y: 400, word: 'hi' },
+    // ]);
+    // startLoop();
 
     const dogImages = ["../assets/normal_dog.svg", "../assets/half_bark_dog.svg", "../assets/bark_dog.svg"];
     let currentIndex = 0;
@@ -106,11 +142,44 @@ export const Game = () => {
 
   useEffect(() => {
     console.log(transcription);
+    console.log(prompts);
+    transcriptionRef.current = transcription;
   }, [transcription]);
 
+  const addRandomSheep = () => {
+    const POSSIBLE_Y: number[] = [250, 350, 450];
+    console.log('spawning sheep');
+
+    const promptsValues = Object.entries(prompts);
+    const i = Math.floor(promptsValues.length * Math.random());
+    const [word, type] = promptsValues[i];
+    const progress = 0.0;
+    const y = POSSIBLE_Y[Math.floor(Math.random() * POSSIBLE_Y.length)];
+    setSheeps(prev => [...prev,
+      {type, progress, y, word}
+    ])
+  }
+
   const stepGame = (): void => {
-    setSheeps((prev) =>
-      prev.map((sheep) => ({ ...sheep, progress: sheep.progress + 0.001 }))
+    setLastSheepTime(prev => {
+      if (prev >= MIN_TIME_BETWEEN_SHEEPS && Math.random() < SHEEP_CHANCE) {
+        addRandomSheep();
+        return 0;
+      }
+      return prev + 100;
+    });
+    setSheeps((prev) => {
+      let newSheeps = prev.map((sheep) => ({ ...sheep, progress: sheep.progress + 0.005 }));
+      newSheeps = newSheeps.filter(sheep => {
+        if (sheep.progress > 0.9) {
+          console.log('sheep escaped!')
+          return false;
+        }
+        return true;
+      })
+      return newSheeps;
+    }
+      
     );
   };
 
@@ -150,6 +219,9 @@ export const Game = () => {
             style={{ backgroundColor: recording ? "red" : "gray" }}
           >
             {recording ? "Stop Recording" : "Record"}
+          </div>
+          <div>
+            {transcription}
           </div>
           <div id="dog">
             <img src={`/src/assets/${dogImage}`} style={{ top: '50%', left: 3, position: 'absolute' }} alt="Dog" />
--- src/components/Sheep.tsx
+++ src/components/Sheep.tsx
@@ -1,30 +1,42 @@
+import SheepWhite from '../assets/sheep_white.svg';
+import SheepBlue from '../assets/sheep_blue.svg';
+import SheepRed from '../assets/sheep_red.svg';
 import '../styles/Sheep.css';
 
 export interface SheepProps {
-  asset: string;
+  type: string;
   progress: number; // percentage
   y: number; // how far down the screen is the sheep
+  word: string;
 }
 
 export const Sheep = (props: SheepProps) => {
   const SHEEP_DIMS = ['4rem', '4rem'];
   const GAME_WIDTH = 960; // todo: sync css and this width?
 
-  const {asset, progress, y} = props;
+  const {type, progress, y, word} = props;
+  const asset = type === 'angry' ? SheepRed : type === 'sad' ? SheepBlue : SheepWhite;
+
 
   // function progress -> x
   const x = (1 - progress) * GAME_WIDTH;
 
   return <>
+    <div className="sheep-container" style={{
+      position: 'absolute',
+      left: x,
+      top: y,
+    }}>
+      <div>
+        {word}
+      </div>
     <img
     src={asset}
     className="sheep"
     style={{
       width: SHEEP_DIMS[0],
       height: SHEEP_DIMS[1],
-      left: x,
-      top: y,
-      position: 'absolute',
     }}/>
+    </div>
   </>
 };
